1fd9358381350989cc38aa593225e17880c71496
Eomhyunjun <zzp1318@gmail.com>
2022-03-01 20:28:46 +0900

    Co-authored-by: 김태훈 <echoja@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 /** @jsxImportSource @emotion/react */
-import './App.css'
-import QnaPage from './QnaPage/QnaPage'
+import { css } from "@emotion/react";
+import "./App.css";
+import { useFloating, shift } from "@floating-ui/react-dom";
 
 function App() {
+  const { x, y, reference, floating, strategy } = useFloating({
+    strategy: 'absolute',
+    placement: 'bottom-end',
+    x: '100px',
+    middleware: [shift()],
+  });
+
   return (
     <div className="App">
-      <QnaPage></QnaPage>
+      <button css={buttonCss} ref={reference}>
+        Button
+      </button>
+      <div
+        ref={floating}
+        style={{
+          position: strategy,
+          top: y ?? '',
+          left: x ?? '30px',
+        }}
+      >
+        Tooltip
+      </div>
     </div>
   );
 }
+
+
+
+const buttonCss = css`
+  position: absolute;
+  top: 200px;
+  left: 300px;
+  background: #222;
+  color: white;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 4px;
+  font-size: 90%;
+  pointer-events: none;
+`;
 
 export default App;
